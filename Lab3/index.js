f5920d3416f94f7eb9721f1360fdba7f6c99708e
msaaddev <mrsaadirfan@gmail.com>
2020-10-28 03:26:45 +0500

    📦 NEW: validate functions

--- Lab3/index.js
+++ Lab3/index.js
@@ -3,6 +3,9 @@
  *
  *  @author Muhammad Saad <https://github.com/msaaddev>
  *	Registration No: 2018-CS-7
+ *
+ * Do npm install to download node packages
+ * Run the program with node index.js
  */
 
 'use-strict';
@@ -24,11 +27,140 @@ class CourseResult {
 
     /**
      *
+     *	checks the course id format
+     *  @param {string} - course id
+     *  @returns {bool}
+     */
+    validateCourseID(id) {
+        if (id.length < 2 || id.length > 8) {
+            return false;
+        }
+
+        for (let i = 0; i < 2; i++) {
+            if (
+                id[i] === '0' ||
+                id[i] === '1' ||
+                id[i] === '2' ||
+                id[i] === '3' ||
+                id[i] === '4' ||
+                id[i] === '5' ||
+                id[i] === '6' ||
+                id[i] === '7' ||
+                id[i] === '8' ||
+                id[i] === '9'
+            ) {
+                return false;
+            }
+        }
+
+        for (let i = 3; i < 5; i++) {
+            if (
+                id[i] !== '0' &&
+                id[i] !== '1' &&
+                id[i] !== '2' &&
+                id[i] !== '3' &&
+                id[i] !== '4' &&
+                id[i] !== '5' &&
+                id[i] !== '6' &&
+                id[i] !== '7' &&
+                id[i] !== '8' &&
+                id[i] !== '9'
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     *	checks the course title
+     *  @param {string} - course title
+     *  @returns {bool}
+     */
+    validateCourseTitle(title) {
+        if (title.length < 10 || title.length > 35) {
+            return false;
+        }
+
+        for (let i = 0; i < title.length; i++) {
+            if (
+                title[i] === '0' ||
+                title[i] === '1' ||
+                title[i] === '2' ||
+                title[i] === '3' ||
+                title[i] === '4' ||
+                title[i] === '5' ||
+                title[i] === '6' ||
+                title[i] === '7' ||
+                title[i] === '8' ||
+                title[i] === '9' ||
+                title[i] === '!' ||
+                title[i] === '@' ||
+                title[i] === '#' ||
+                title[i] === '$' ||
+                title[i] === '%' ||
+                title[i] === '^' ||
+                title[i] === '&' ||
+                title[i] === '*'
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     *	checks the credit hours
+     *  @param {int} - course credit hours
+     *  @returns {bool}
+     */
+    validateCreditHrs(hrs) {
+        if (hrs < 1 || hrs > 3) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     *	checks the credit marks
+     *  @param {int} - course marks
+     *  @returns {bool}
+     */
+    validateMarks(marks) {
+        if (marks < 0 || marks > 100) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     *	checks the semester
+     *  @param {int} - course semester
+     *  @returns {bool}
+     */
+    validateSem(sem) {
+        if (sem < 1 || sem > 8) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
      *	takes course id from the user
      */
     async setCourseID() {
         console.log(`ENTER YOUR COURSE ID: `);
         this.courseNameSpace.courseID = await io.read();
+
+        while (!this.validateCourseID(this.courseNameSpace.courseID)) {
+            console.log(`INVALID ID: PLEASE ENTER COURSE ID AGAIN: `);
+            this.courseNameSpace.courseID = await io.read();
+        }
     }
 
     /**
@@ -38,6 +170,11 @@ class CourseResult {
     async setCourseTitle() {
         console.log(`ENTER YOUR COURSE TITLE: `);
         this.courseNameSpace.courseTitle = await io.read();
+
+        while (!this.validateCourseTitle(this.courseNameSpace.courseTitle)) {
+            console.log(`INVALID TITLE: PLEASE ENTER COURSE TITLE AGAIN: `);
+            this.courseNameSpace.courseTitle = await io.read();
+        }
     }
 
     /**
@@ -47,6 +184,11 @@ class CourseResult {
     async setCreditHrs() {
         console.log(`ENTER YOUR COURSE CREDIT HRS: `);
         this.courseNameSpace.creditHrs = await io.read();
+
+        while (!this.validateCreditHrs(this.courseNameSpace.creditHrs)) {
+            console.log(`INVALID CREDIT HRS: PLEASE ENTER COURSE CREDIT HRS AGAIN: `);
+            this.courseNameSpace.creditHrs = await io.read();
+        }
     }
 
     /**
@@ -56,6 +198,11 @@ class CourseResult {
     async setMarks() {
         console.log(`ENTER YOUR COURSE MARKS: `);
         this.courseNameSpace.marks = await io.read();
+
+        while (!this.validateMarks(this.courseNameSpace.marks)) {
+            console.log(`INVALID MARKS: PLEASE ENTER COURSE MARKS AGAIN: `);
+            this.courseNameSpace.marks = await io.read();
+        }
     }
 
     /**
@@ -65,6 +212,11 @@ class CourseResult {
     async setSemester() {
         console.log(`ENTER THE COURSE SEMESTER: `);
         this.courseNameSpace.semester = await io.read();
+
+        while (!this.validateSem(this.courseNameSpace.semester)) {
+            console.log(`INVALID SEMESTER: PLEASE ENTER THE COURSE SEMESTER AGAIN: `);
+            this.courseNameSpace.semester = await io.read();
+        }
     }
 
     /**
@@ -201,11 +353,139 @@ class Student {
 
     /**
      *
+     *	checks that student name is only comprised of alphabet
+     *  @param {string} - student name
+     *  @returns {bool}
+     */
+    validateName(name) {
+        for (let i = 0; i < name.length; i++) {
+            if (
+                name[i] === '0' ||
+                name[i] === '1' ||
+                name[i] === '2' ||
+                name[i] === '3' ||
+                name[i] === '4' ||
+                name[i] === '5' ||
+                name[i] === '6' ||
+                name[i] === '7' ||
+                name[i] === '8' ||
+                name[i] === '9' ||
+                name[i] === '!' ||
+                name[i] === '@' ||
+                name[i] === '#' ||
+                name[i] === '$' ||
+                name[i] === '%' ||
+                name[i] === '^' ||
+                name[i] === '&' ||
+                name[i] === '*'
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     *	checks that student registration format
+     *  @param {string} - student registration no#
+     *  @returns {bool}
+     */
+    validateRegNum(regNum) {
+        for (let i = 0; i < 4; i++) {
+            if (
+                regNum[i] !== '0' &&
+                regNum[i] !== '1' &&
+                regNum[i] !== '2' &&
+                regNum[i] !== '3' &&
+                regNum[i] !== '4' &&
+                regNum[i] !== '5' &&
+                regNum[i] !== '6' &&
+                regNum[i] !== '7' &&
+                regNum[i] !== '8' &&
+                regNum[i] !== '9'
+            ) {
+                return false;
+            }
+        }
+
+        if (regNum[4] !== '-') {
+            return false;
+        }
+
+        for (let i = 5; i < 7; i++) {
+            if (
+                regNum[i] === '0' ||
+                regNum[i] === '1' ||
+                regNum[i] === '2' ||
+                regNum[i] === '3' ||
+                regNum[i] === '4' ||
+                regNum[i] === '5' ||
+                regNum[i] === '6' ||
+                regNum[i] === '7' ||
+                regNum[i] === '8' ||
+                regNum[i] === '9' ||
+                regNum[i] === '!' ||
+                regNum[i] === '@' ||
+                regNum[i] === '#' ||
+                regNum[i] === '$' ||
+                regNum[i] === '%' ||
+                regNum[i] === '^' ||
+                regNum[i] === '&' ||
+                regNum[i] === '*'
+            ) {
+                return false;
+            }
+        }
+
+        if (regNum[7] !== '-') {
+            return false;
+        }
+
+        for (let i = 8; i < regNum.length; i++) {
+            if (
+                regNum[i] !== '0' &&
+                regNum[i] !== '1' &&
+                regNum[i] !== '2' &&
+                regNum[i] !== '3' &&
+                regNum[i] !== '4' &&
+                regNum[i] !== '5' &&
+                regNum[i] !== '6' &&
+                regNum[i] !== '7' &&
+                regNum[i] !== '8' &&
+                regNum[i] !== '9'
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     *	checks that degree is either MS, BS, BE
+     *  @param {string} - degree
+     *  @returns {bool}
+     */
+    validateDegree(degree) {
+        if (degree !== 'MS' && degree !== 'BS' && degree !== 'BE') {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
      *	takes student name from the student
      */
     async setName() {
         console.log(`\nENTER STUDENT NAME: `);
         this.studentNameSpace.name = await io.read();
+
+        while (!this.validateName(this.studentNameSpace.name)) {
+            console.log(`INVALID NAME: PLEASE ENTER STUDENT NAME AGAIN: `);
+            this.studentNameSpace.name = await io.read();
+        }
     }
 
     /**
@@ -215,6 +495,11 @@ class Student {
     async setRegNum() {
         console.log(`ENTER STUDENT REG NO#: `);
         this.studentNameSpace.regNum = await io.read();
+
+        while (!this.validateRegNum(this.studentNameSpace.regNum)) {
+            console.log(`INVALID REG NO#: PLEASE ENTER STUDENT REG NO# AGAIN: `);
+            this.studentNameSpace.regNum = await io.read();
+        }
     }
 
     /**
@@ -224,6 +509,11 @@ class Student {
     async setDegree() {
         console.log(`ENTER STUDENT DEGREE: `);
         this.studentNameSpace.degree = await io.read();
+
+        while (!this.validateDegree(this.studentNameSpace.degree)) {
+            console.log(`INVALID DEGREE: PLEASE ENTER STUDENT DEGREE AGAIN: `);
+            this.studentNameSpace.degree = await io.read();
+        }
     }
 
     /**
@@ -234,6 +524,17 @@ class Student {
         const temp = new CourseResult();
         await temp.getInput();
         this.studentNameSpace.courses.push(temp);
+    }
+
+    /**
+     *
+     *	takes all the required input
+     */
+    async getInput() {
+        await this.setName();
+        await this.setRegNum();
+        await this.setDegree();
+        await this.setCourse();
     }
 
     /**
@@ -259,12 +560,6 @@ class Student {
     getDegree() {
         return this.studentNameSpace.degree;
     }
-
-    /**
-     *
-     *	logs student courses on the terminal
-     */
-    getCourses() {}
 
     /**
      *
@@ -452,7 +747,7 @@ class Student {
         studentInfo.push([`Session: ${this.getSession()}`]);
 
         console.log('\n');
-        console.log( studentInfo.toString());
+        console.log(studentInfo.toString());
 
         for (let i = 0; i < sem.length; i++) {
             const semCourses = this.coursesOfSem(sem[i]);
